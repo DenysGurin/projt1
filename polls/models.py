9f31ff5d047e5a78d4b3af306787ee1cdcfce92f
DenysGurin <gurindenys@gmail.com>
2017-03-14 17:57:08 +0200

    Start work on content

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
-class Polls(models.Model):
-	name = models.CharField(max_length=30)
-	picture = models.CharField(max_length=60)
+class Poll(models.Model):
+	name = models.CharField(max_length=30, null=True)
+	description = models.CharField(max_length=300, null=True)
+	category = models.CharField(max_length=60, null=True)
+	picture = models.CharField(max_length=60, null=True)
 	pub_date = models.DateTimeField('date published')
--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.views.generic.base import View
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
-
+from .models import Poll
 
 
 class Polls(View):
@@ -11,6 +11,14 @@ class Polls(View):
 	# @login_required()
     def get(self, request):
     	if request.user.is_authenticated():
-    		return render(request, 'polls.html', {})
+    		polls = Poll.objects
+    		context = {"polls" : polls}
+    		return render(request, 'polls.html', context)
+    		# return HttpResponse([poll for poll in polls.all()])
     	else:
     		return redirect('/')
+    def post():
+    	polls = Poll.objects[:8]
+    	context = {"polls" : polls}
+    	return render(request, "polls", context)
+    	#return HttpResponse(polls)
